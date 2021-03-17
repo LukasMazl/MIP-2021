0f26a5672bbe550636d670cffbdba2418618a422
Lukas Mazl <lukas.mazl@plus4u.net>
2021-03-17 10:10:26 +0100

    Update

--- cv02/main.cpp
+++ cv02/main.cpp
@@ -5,11 +5,10 @@
 #include "F469_GUI.hpp"
 #include <string>
 
-#define START_TIME 68
+#define START_TIME 120
 
 volatile static int COUNTDOWN = START_TIME;
-Thread thread;
-bool thread_is_done = false;
+volatile static bool thread_is_done = false;
 
 string timeToString(int time) {
     int hour = time / 3600;
@@ -20,7 +19,7 @@ string timeToString(int time) {
 } 
 
 void countdown() {
-    while(COUNTDOWN >= 0) {
+    while(COUNTDOWN > 0) {
         COUNTDOWN--;
         ThisThread::sleep_for(1s);
     }
@@ -30,7 +29,7 @@ void countdown() {
 int main()
 {
     BSP_LED_Init(DISCO_LED_BLUE);
-
+    
     Label obj10(400, 20, "Cviceni 2: Budikobomba", Label::CENTER, Font16);
     
     Label time(400, 200, "00:00:00", Label::CENTER, Font24);
@@ -44,8 +43,17 @@ int main()
 
     Button resetButton(10, 354, 100, 80, "Reset");
 
+    // Watchdog &watchdog = Watchdog::get_instance();
+    // watchdog.start(COUNTDOWN * 1000 + 2000 * 5);
 
+    string boomText = "boooom";
+
+    int interation_count = 0;
+    int expresion_count = 0;
+
+    Thread thread;
     thread.start(countdown);
+
     while (true)
     {
         if (addMinute.Touched()) {
@@ -69,27 +77,36 @@ int main()
         }
 
         if (resetButton.Touched()) {
-            COUNTDOWN = START_TIME;
             resetButton.Activate();
+            COUNTDOWN = START_TIME;
             if(thread_is_done) {
                 thread.start(countdown);
                 thread_is_done = false;
             }
         }
 
-        if(COUNTDOWN >= 0) {
-            string timeString = timeToString(COUNTDOWN);
-            time.Draw(timeString);
-
-            ThisThread::sleep_for(200ms);
+        string outputline;
+        if (COUNTDOWN > 0) {
+            outputline = timeToString(COUNTDOWN);
         } else {
-            //Do animation
-            string boomText = "boooom";
-            for(int i = 0; i < 10; i++) {
-                time.Draw(boomText);
+            
+            if (interation_count != 0 && interation_count % 10 == 0) {
+                interation_count = 0;
                 boomText = "!!" + boomText + "!!";
-                ThisThread::sleep_for(2s);
+                expresion_count++;
             }
+
+            if(expresion_count > 5) {
+                expresion_count = 0;
+                boomText = "boooom";
+            }
+            
+            interation_count++;
+
+            outputline = boomText;
         }
+
+        time.Draw(outputline);
+        ThisThread::sleep_for(200ms);
     }
 }
